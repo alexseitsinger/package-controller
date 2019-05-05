2eb2f30a45d4b9dd77c52f8e7af1c4d0392b9cc6
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-05 19:47:59 +0000

    feat: renamed exceptions, added new one for util

---
+++ src/package_controller/exceptions.py
@@ -0,0 +1,14 @@
+class VersionPartNameException(Exception):
+    pass
+
+
+class ReadFileException(Exception):
+    pass
+
+
+class FindInitModuleException(Exception):
+    pass
+
+
+class GetPackageLongDescriptionException(Exception):
+    pass
