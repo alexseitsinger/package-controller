a8f17ba61c67b4e233d001987aea7936dc3a076a
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-05 18:43:45 +0000

    chore: replaced setuptools setup() with our custom package_setup() script

--- setup.py
+++ setup.py
@@ -1,42 +1,14 @@
 #!/usr/bin/env python
 # -*- encoding: utf-8 -*-
-import os
-import re
-from setuptools import setup, find_packages
-
-HERE = os.path.abspath(os.path.dirname(__file__))
-NAME = os.path.basename(HERE)
+from setup_utils import package_setup
 
 
-def read(*parts):
-    with open(os.path.join(HERE, *parts), "r", encoding="utf-8") as fp:
-        return fp.read()
-
-
-def find_version(*file_paths):
-    version_file = read(*file_paths)
-    version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", version_file, re.M)
-    if version_match:
-        return version_match.group(1)
-    raise RuntimeError("Unable to find version string.")
-
-
-setup(
-    name=NAME,
-    version=find_version("src", NAME, "__init__.py"),
-    description="",
-    long_description=read("README.md"),
-    long_description_content_type="text/markdown",
-    url="https://github.com/alexseitsinger/{}".format(NAME),
+package_setup(
+    description="A simple package controller for automating setups, versioning, etc.",
+    base_url="https://github.com/alexseitsinger",
     author="Alex Seitsinger",
     author_email="contact@alexseitsinger.com",
-    classifiers=[
-        "License :: OSI Approved :: BSD License",
-        "Programming Language :: Python :: 3",
-    ],
-    package_dir={"": "src"},
-    packages=find_packages("src", exclude=["tests"]),
-    license="BSD 2-Clause License",
-    install_requires=[],
-    include_package_data=True,
+    install_requires=["click", "semver"],
+    console_scripts={"pc": "cli:main"}
 )
+
