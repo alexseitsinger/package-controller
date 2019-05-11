e54e4f18e36299325a5d387ee7a1615f655d99b1
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-11 22:01:07 +0000

    feat: adds git updates
    adds git updates to version bumps

--- src/package_controller/cli/version.py
+++ src/package_controller/cli/version.py
@@ -5,32 +5,77 @@ from ..utils import (
     get_package_version,
     save_file,
     find_init_module,
+    git_update,
 )
 from ..settings import DEFAULT_VERSION_VARIABLE
 
 
 @click.command()
-@click.option("--variable", default=DEFAULT_VERSION_VARIABLE, required=False)
-@click.option("--patch", default=False, required=False, is_flag=True)
-@click.option("--minor", default=False, required=False, is_flag=True)
-@click.option("--major", default=False, required=False, is_flag=True)
-def version(major, minor, patch, variable):
+@click.option(
+    "--variable",
+    default=DEFAULT_VERSION_VARIABLE,
+    required=False,
+    help="The version variable in the file.")
+@click.option("--patch", default=False, required=False, is_flag=True, help="Updates the patch version.")
+@click.option("--minor", default=False, required=False, is_flag=True, help="Updates the minor version.")
+@click.option("--major", default=False, required=False, is_flag=True, help="Updates the major version.")
+@click.option("--git", default=False, required=False, is_flag=True, help="Updates git with new tag and changelog for version.")
+def version(major, minor, patch, variable, git):
     current_version = get_package_version(variable=variable)
     init_module = find_init_module()
-    if any([ x is True for x in [ major, minor, patch ]]):
-        if major is True and all([ x is False for x in [ minor, patch ]]):
+    if any([x is True for x in [major, minor, patch]]):
+        if major is True and all([x is False for x in [minor, patch]]):
             next_version = semver.bump_major(current_version)
             save_file(init_module, variable, next_version)
-            click.echo("Successfully updated major version from {} to {}".format(current_version, next_version))
-        elif minor is True and all([ x is False for x in [ major, patch ]]):
+            if git is True:
+                git_update(
+                    init_module=init_module,
+                    current_version=current_version,
+                    next_version=next_version
+                )
+            click.secho(
+                "Successfully updated major version from {} to {}".format(
+                    current_version, next_version
+                ),
+                fg="green"
+            )
+        elif minor is True and all([x is False for x in [major, patch]]):
             next_version = semver.bump_minor(current_version)
             save_file(init_module, variable, next_version)
-            click.echo("Successfully updated minor version from {} to {}".format(current_version, next_version))
-        elif patch is True and all([ x is False for x in [ major, minor ]]):
+            if git is True:
+                git_update(
+                    init_module=init_module,
+                    current_version=current_version,
+                    next_version=next_version
+                )
+            click.secho(
+                "Successfully updated minor version from {} to {}".format(
+                    current_version, next_version
+                ),
+                fg="green"
+            )
+        elif patch is True and all([x is False for x in [major, minor]]):
             next_version = semver.bump_patch(current_version)
             save_file(init_module, variable, next_version)
-            click.echo("Successfully updated patch version from {} to {}".format(current_version, next_version))
+            if git is True:
+                git_update(
+                    init_module=init_module,
+                    current_version=current_version,
+                    next_version=next_version
+                )
+            click.secho(
+                "Successfully updated patch version from {} to {}".format(
+                    current_version, next_version
+                ),
+                fg="green"
+            )
         else:
-            click.echo("Can only update one version major, minor, or patch")
+            click.secho(
+                "Can only update one version: major, minor, or patch",
+                fg="red"
+            )
     else:
-        click.echo("Current version: {}".format(current_version))
+        click.secho(
+            "Current version: {}".format(current_version),
+            fg="yellow",
+        )
