a18db13d53861287bb7da0dc140df2aae3a805c7
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-12 01:01:26 +0000

    refactor: changes method name
    changes method name to someone shorter

--- src/package_controller/utils/get_version.py
+++ src/package_controller/utils/get_version.py
@@ -5,7 +5,7 @@ from .find_init_module import find_init_module
 from ..settings import DEFAULT_VERSION_VARIABLE
 
 
-def get_package_version(variable=DEFAULT_VERSION_VARIABLE):
+def get_version(variable=DEFAULT_VERSION_VARIABLE):
     init_module = find_init_module()
     package_version = read_file(init_module, variable)
     return package_version
