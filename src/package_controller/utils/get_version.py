695e53f16f4f692e3258839f111fac972818e9b8
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-12 21:35:55 +0000

    refactor: optimizes method
    changed method to automatically find init module, and uses version variable automatically, instead of being passed it.

--- src/package_controller/utils/get_version.py
+++ src/package_controller/utils/get_version.py
@@ -2,11 +2,10 @@ import os
 
 from .read_file import read_file
 from .find_init_module import find_init_module
-from ..settings import DEFAULT_VERSION_VARIABLE
 
 
-def get_version(variable=DEFAULT_VERSION_VARIABLE):
+def get_version():
     init_module = find_init_module()
-    package_version = read_file(init_module, variable)
-    return package_version
+    version = read_file(init_module, "__version__")
+    return version
 
