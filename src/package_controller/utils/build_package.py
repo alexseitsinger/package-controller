dba49f1deb45091ec0fe179fa57c590ace749c25
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-13 17:23:26 +0000

    refactor: updates util method

--- src/package_controller/utils/build_package.py
+++ src/package_controller/utils/build_package.py
@@ -1,25 +1,36 @@
 import os
-import click
 
 from .run import run
+from .get_version import get_version
+from .find_file import find_file
 
 BUILD_ARGS = ["python", "setup.py", "sdist", "bdist_wheel"]
 PIPENV_RUN_ARGS = ["pipenv", "run"]
+TARBALL_NAME = "{}-{}.tar.gz"
+WHEEL_NAME = "{}-{}-py3-none-any.whl"
+
 
 def build_package():
+    current_version = get_version()
+    setup_file = find_file("setup.py")
+    root = os.path.dirname(setup_file)
+    name = os.path.basename(root)
+    dist_dir = os.path.join(root, "dist")
+    wheel_name = WHEEL_NAME.format(name, current_version)
+    wheel = os.path.join(dist_dir, wheel_name)
+    tarball_name = TARBALL_NAME.format(name, current_version)
+    tarball = os.path.join(dist_dir, tarball_name)
+    if os.path.exists(wheel):
+        raise RuntimeError("Wheel already exists. ({})".format(
+            os.path.basename(wheel)
+        ))
+    if os.path.exists(tarball):
+        raise RuntimeError("Tarball already exists. ({})".format(
+            os.path.basename(tarball)
+        ))
     try:
-        out = run(*BUILD_ARGS)
-        click.secho("Successfully built package.", fg="green")
-        return out
+        return run(*BUILD_ARGS)
     except RuntimeError:
-        try:
-            args = PIPENV_RUN_ARGS + BUILD_ARGS
-            out = run(*args)
-            click.secho("Successfully built package.", fg="green")
-            return out
-        except Exception:
-            return click.secho("Failed to build package.", fg="red")
-
-
-
+        args = PIPENV_RUN_ARGS + BUILD_ARGS
+        return run(*args)
 
