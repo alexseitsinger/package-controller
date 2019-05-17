2e018acc597997e737d30c322f1215c1801d6225
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-17 23:01:16 +0000

    refactor: adds check for executable
    throws an exception if the executable doesnt exist

--- src/package_controller/utils/make_changelog.py
+++ src/package_controller/utils/make_changelog.py
@@ -1,9 +1,13 @@
 import os
 
 from .run import run
+from .which import which
 
 
 def make_changelog(output="CHANGELOG.md", type="angular", style="angular"):
+    git_changelog = which("git-changelog")
+    if git_changelog is None:
+        raise RuntimeError("git-changelog is not installed.")
     run("git-changelog", ".", "-o", output, "-t", type, "-s", style)
     changelog = os.path.join(os.getcwd(), output)
     return changelog
