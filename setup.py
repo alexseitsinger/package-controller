8068de1a3c324953c9633df387b4574aacb267c9
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-17 18:48:06 +0000

    chore: adds and updates sections to the setup

--- setup.py
+++ setup.py
@@ -3,24 +3,36 @@
 from setuptools import setup, find_packages
 from setup_utils import PACKAGE_NAME, read, read_section
 
+URL = "https://github.com/alexseitsinger/{}".format(PACKAGE_NAME)
+README_NAME = "README.md"
 
 setup(
     name=PACKAGE_NAME,
     version=read(("src", PACKAGE_NAME, "__init__.py",), "__version__"),
-    description=read_section(("README.md",), "Description", (0,)),
-    long_description=read(("README.md",)),
+    description=read_section((README_NAME,), "Description", (0,)),
+    long_description=read((README_NAME,)),
     long_description_content_type="text/markdown",
     author="Alex Seitsinger",
     author_email="contact@alexseitsinger.com",
-    url="https://github.com/alexseitsinger/{}".format(PACKAGE_NAME),
+    url=URL,
     install_requires=["click", "semver"],
     entry_points={"console_scripts": ["pc={}.cli:main".format(PACKAGE_NAME)]},
     package_dir={"": "src"},
     packages=find_packages("src", exclude=["tests"]),
     include_package_data=True,
     license="BSD 2-Clause License",
+    keywords=["package", "semantic version", "git", "distribution"],
     classifiers=[
         "License :: OSI Approved :: BSD License",
         "Programming Language :: Python :: 3",
-    ]
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+    ],
+    project_urls={
+        "Source": URL,
+        "Tracker": "{}/issues".format(URL)
+    }
 )
