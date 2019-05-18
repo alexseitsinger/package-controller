8682397486f275c96f2cfc2611b14549e329e346
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-18 00:24:06 +0000

    feat: adds tag deleting if points to bad commit
    if the commit doesnt exist, we attempt to delete the tag, and re-run the tag attempt

--- src/package_controller/utils/git_tag.py
+++ src/package_controller/utils/git_tag.py
@@ -1,7 +1,8 @@
 from .run import run
-
+from .assert_commit import assert_commit
 
 GIT_TAG_ARGS = ["git", "tag"]
+
 
 def git_tag(name, hash=None):
     if not name.startswith("v"):
@@ -9,5 +10,19 @@ def git_tag(name, hash=None):
     args = GIT_TAG_ARGS + [name]
     if hash is not None:
         args.append(hash)
-    return run(*args)
+    out = run(*args)
+    # If we get some output and it says the tag exists, continue...
+    if out and out.startswith("fatal: tag") and out.endswith("already exists"):
+        # Get the commit hash that is connected to the tag.
+        tagged_commit = run("git", "rev-list", "-n", 1, name)
+        # If the commit does not exist, then delete the tag, and re-run this command.
+        # Otherwise, raise an exception.
+        if not assert_commit(tagged_commit):
+            run(*GIT_TAG_ARGS + ["-d", name])
+            print("Deleting old tag that points to non-existant commit. ({}, {})".format(name, tagged_commit))
+            return git_tag(name=name, hash=hash)
+        else:
+            raise RuntimeError("That tag already exists on a valid commit. ({}, {})".format(name, tagged_commit))
+    return out
+
 
