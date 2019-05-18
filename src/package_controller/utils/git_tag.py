ab98fe9ba7be8c12f5db7ad7f366882d743b48d0
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-18 00:51:07 +0000

    fix: fixes git tag exception catching

--- src/package_controller/utils/git_tag.py
+++ src/package_controller/utils/git_tag.py
@@ -10,19 +10,23 @@ def git_tag(name, hash=None):
     args = GIT_TAG_ARGS + [name]
     if hash is not None:
         args.append(hash)
-    out = run(*args)
-    # If we get some output and it says the tag exists, continue...
-    if out and out.startswith("fatal: tag") and out.endswith("already exists"):
-        # Get the commit hash that is connected to the tag.
-        tagged_commit = run("git", "rev-list", "-n", 1, name)
-        # If the commit does not exist, then delete the tag, and re-run this command.
-        # Otherwise, raise an exception.
-        if not assert_commit(tagged_commit):
-            run(*GIT_TAG_ARGS + ["-d", name])
-            print("Deleting old tag that points to non-existant commit. ({}, {})".format(name, tagged_commit))
-            return git_tag(name=name, hash=hash)
+    try:
+        return run(*args)
+    except RuntimeError as exc:
+        message = str(exc)
+        # If we get some output and it says the tag exists, continue...
+        if message.startswith("fatal: tag") and message.endswith("already exists"):
+            # Get the commit hash that is connected to the tag.
+            tagged_commit = run("git", "rev-list", "-n", 1, name)
+            # If the commit does not exist, then delete the tag, and re-run this command.
+            # Otherwise, raise an exception.
+            if not assert_commit(tagged_commit):
+                run(*GIT_TAG_ARGS + ["-d", name])
+                print("Deleting old tag that points to non-existant commit. ({}, {})".format(name, tagged_commit))
+                return git_tag(name=name, hash=hash)
+            else:
+                raise RuntimeError("That tag already exists on a valid commit. ({}, {})".format(name, tagged_commit))
         else:
-            raise RuntimeError("That tag already exists on a valid commit. ({}, {})".format(name, tagged_commit))
-    return out
+            raise exc
 
 
