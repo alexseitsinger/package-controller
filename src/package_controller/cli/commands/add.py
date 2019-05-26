a276ac8e4b4348c1c5467a916caa3018d28ef932
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-26 18:34:20 +0000

    feat: adds add command to cli

---
+++ src/package_controller/cli/commands/add.py
@@ -0,0 +1,15 @@
+import os
+import click
+
+from ...utils.git_add import git_add
+
+
+@click.command()
+@click.argument("name", required=True)
+def commit(name):
+    try:
+        git_add(name)
+        click.secho("Successfully added files.", fg="green", bold=True)
+    except RuntimeError as exc:
+        click.secho("Failed to add files.", fg="red", bold=True)
+        click.secho(str(exc), fg="red")
