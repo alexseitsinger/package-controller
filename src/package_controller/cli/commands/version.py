494b4ca8a6fbef50833f1cee7b30b739ca5398e7
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-26 18:32:30 +0000

    feat: adds force flag opt. changes imports.

--- src/package_controller/cli/commands/version.py
+++ src/package_controller/cli/commands/version.py
@@ -1,26 +1,35 @@
 import click
 import semver
 
-from ...utils import (
-    git_update,
-    bump_version,
-    get_version,
-)
+from ...utils.git_update import git_update
+from ...utils.bump_version import bump_version
+from ...utils.get_version import get_version
 
 
 @click.command()
-@click.option("--major", default=False, required=False, is_flag=True, help="Updates the major version.")
-@click.option("--minor", default=False, required=False, is_flag=True, help="Updates the minor version.")
-@click.option("--patch", default=False, required=False, is_flag=True, help="Updates the patch version.")
-@click.option("--git", default=True, required=False, is_flag=True, help="Updates git with new tag and changelog for version.")
-def version(major, minor, patch, git):
+@click.option(
+    "--major", default=False, required=False, is_flag=True,
+    help="Updates the major version.")
+@click.option(
+    "--minor", default=False, required=False, is_flag=True,
+    help="Updates the minor version.")
+@click.option(
+    "--patch", default=False, required=False, is_flag=True,
+    help="Updates the patch version.")
+@click.option(
+    "--no-git", default=False, required=False, is_flag=True,
+    help="Do not update git with a new tag and changelog for version.")
+@click.option(
+    "--force", default=False, required=False, is_flag=True,
+    help="Force the version update even if there are uncommitted changes.")
+def version(major, minor, patch, no_git, force):
     if any([x is True for x in [major, minor, patch]]):
         try:
-            old_version, new_version = bump_version(major, minor, patch)
+            old_version, new_version = bump_version(
+                major=major, minor=minor, patch=patch, force=force)
             message = "Successfully updated version from {} to {}".format(
-                old_version, new_version
-            )
-            if git is True:
+                old_version, new_version)
+            if no_git is False:
                 git_update(old_version, new_version)
             click.secho(message, fg="green", bold=True)
         except RuntimeError as exc:
