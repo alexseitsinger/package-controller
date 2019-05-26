cc1b700befc3341eac822a28f72d0459bddd5037
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-26 18:37:08 +0000

    feat: adds msg formatting and checking

--- src/package_controller/utils/git_commit.py
+++ src/package_controller/utils/git_commit.py
@@ -1,14 +1,66 @@
+import textwrap
+import re
+
 from .run import run
 
 
 GIT_COMMIT_ARGS = ["git", "commit"]
 GIT_LAST_COMMIT_HASH_ARGS = ["git", "rev-parse", "HEAD"]
+RE_DESCRIPTION_DELIMITER = r"(?![\w\d])\.\s+"
+COMMIT_TYPES_ALLOWED = [
+    "chore", "feat", "fix", "docs", "style", "refactor", "perf", "localize",
+    "test",
+]
+
+
+def assert_commit_type(name):
+    if not name in COMMIT_TYPES_ALLOWED:
+        raise RuntimeError("The commit type must be one of {}".format(
+            ", ".join(COMMIT_TYPES_ALLOWED)))
+
+
+def format_commit_text(text):
+    text = text.strip()
+    text = text.capitalize()
+    if not text.endswith("."):
+        text = "{}.".format(text)
+    return text
+
+
+def format_commit_description(description, delimiter="."):
+    # Try to split the description up into bits using the delimiter.
+    bits = None
+    if bits is None or isinstance(bits, list) and not len(bits):
+        bits = [
+            format_commit_text(x)
+            for x in re.split(RE_DESCRIPTION_DELIMITER, description)
+            if len(x)
+        ]
+    # convert the bits into a single string again, that is wrapped at 72 
+    # characters.
+    text = " ".join(bits)
+    wrapped = textwrap.wrap(text, width=72)
+    joined = "\n".join(wrapped)
+    return joined
+
 
 def git_commit(type, subject, description=None):
-    header = "{type}: {subject}".format(type=type, subject=subject)
-    commit_args = GIT_COMMIT_ARGS + ["-m", header]
+    assert_commit_type(type)
+    # Remove any excess whitespace from the subject.
+    subject = format_commit_text(subject)
+    # Create the heading for the commit.
+    heading = "{type}: {subject}".format(type=type, subject=subject)
+    if len(heading) > 50:
+        raise RuntimeError(
+            "The commit heading must be 50 characters or less.")
+    # Create the commit args.
+    commit_args = GIT_COMMIT_ARGS + ["-m", heading]
+    # Iterate over the description, if possible, and convert each line into
+    # a new -m entry to use a newline in the commit message.
     if description is not None:
-        commit_args += ["-m", description]
+        commit_args += format_commit_description(description)
+    # Run the command we've put togehter to create the commit.
     run(*commit_args)
+    # Then, get and return the hash for the commit we just created.
     return run(*GIT_LAST_COMMIT_HASH_ARGS)
 
