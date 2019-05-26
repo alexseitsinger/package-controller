73731fd104d0193f0ae1e9bc80bd56119c5b81c5
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-26 18:25:35 +0000

    refactor: combines run args together

--- src/package_controller/utils/git_add.py
+++ src/package_controller/utils/git_add.py
@@ -2,9 +2,9 @@ from .run import run
 
 GIT_ADD_ARGS = ["git", "add"]
 
+
 def git_add(*files):
     if not len(files):
         raise RuntimeError("No files passed")
-    args = GIT_ADD_ARGS + list(files)
-    return run(*args)
+    return run(*GIT_ADD_ARGS + list(files))
 
