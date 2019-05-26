6b8347ed13d2ef5fc945b197edb96dd7b886f99c
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-26 21:15:02 +0000

    style: Adds newline to file.

--- src/package_controller/utils/git_push.py
+++ src/package_controller/utils/git_push.py
@@ -3,6 +3,7 @@ from .run import run
 GIT_PUSH_ARGS = ["git", "push"]
 GIT_PUSH_TAGS_ARGS = ["--tags"]
 
+
 def git_push(remote="origin", branch="master", tags=False):
     if tags is True:
         args = GIT_PUSH_ARGS + GIT_PUSH_TAGS_ARGS
