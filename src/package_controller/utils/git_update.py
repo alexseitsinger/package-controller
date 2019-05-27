f416615caa743c620a09e4474a9ae5d30508f958
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-27 14:27:09 -0400

    refactor: Adds changelog before tag.

--- src/package_controller/utils/git_update.py
+++ src/package_controller/utils/git_update.py
@@ -7,31 +7,25 @@ from .run import run
 
 
 def git_update(current_version, next_version):
-    # save the last commit hash.
+    # save the last commit hash in case we need to reset.
     last_commit_hash = run("git", "rev-parse", "HEAD")
-    # Get the init module that the version is saved to.
+
+    # Create the release commit.
     init_module = find_init_module()
-    # Add it to git.
-    git_add(init_module)
-    # Commit the change, and get the new hash.
+    changelog = make_changelog()
+    git_add(init_module, changelog)
     commit_hash = git_commit(
-        commit_type="docs",
-        subject="updates the version from {} to {}".format(
+        commit_type="chore",
+        subject=next_version,
+        description="Updates the version from {} to {}".format(
             current_version, next_version
-        ),
+        )
     )
+
     # Create a new tag pointing to this commit.
-    tag_name = "v{}".format(next_version)
     try:
+        tag_name = "v{}".format(next_version)
         git_tag(tag_name, commit_hash)
     except RuntimeError as exc:
         run("git", "reset", "--hard", last_commit_hash)
         raise exc
-    # Try to create the changelog.
-    changelog = make_changelog()
-    git_add(changelog)
-    git_commit(
-        commit_type="docs",
-        subject="updates the changelog for {}".format(tag_name),
-    )
-
