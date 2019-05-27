9021b139ca9e20772e168be66cb8b4bb0ef974b7
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-27 11:48:25 -0400

    refactor: Condenses func into single line.

--- src/package_controller/utils/get_version.py
+++ src/package_controller/utils/get_version.py
@@ -5,7 +5,5 @@ from .find_init_module import find_init_module
 
 
 def get_version():
-    init_module = find_init_module()
-    version = read_file(init_module, "__version__")
-    return version
+    return read_file(find_init_module(), "__version__")
 
