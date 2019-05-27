08798ce74d73c3f725dc4ab677beeb07600a6d6c
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-27 00:51:57 +0000

    docs: Updates the version from 0.3.10 to 0.4.0.

--- src/package_controller/__init__.py
+++ src/package_controller/__init__.py
@@ -1,1 +1,4 @@
-__version__ = "0.3.10"
+# Do not change this version manually.
+# Versioning is managed by package_controller.
+# To update the version run `pc version --patch | --minor | --major`
+__version__ = "0.4.0"
