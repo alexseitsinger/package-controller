8880ae182ba496e74e4a29054a393c2ecd39d815
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-27 00:45:19 +0000

    refactor: Changes save_file to replace_line.

---
+++ src/package_controller/utils/replace_line.py
@@ -0,0 +1,31 @@
+import re
+from io import open
+from tempfile import mkstemp
+from shutil import move
+from os import fdopen, remove
+
+
+def replace_line(path, pattern, replacement, prepended_lines=[]):
+    total_prepended = len(prepended_lines)
+    fh, abs_path = mkstemp()
+    with fdopen(fh, "w") as new_file:
+        with open(path) as old_file:
+            old_lines = old_file.readlines()
+            for old_line in old_lines:
+                match = re.match(pattern, old_line)
+                if match:
+                    start_index = old_lines.index(old_line)
+                    count = -1
+                    for prepended_line in prepended_lines:
+                        count += 1
+                        place = min(start_index, max(0, (start_index - (total_prepended - count))))
+                        try:
+                            if old_lines[place] != prepended_line:
+                                new_file.write(prepended_line)
+                        except IndexError:
+                            new_file.write(prepended_line)
+                    new_file.write(replacement)
+                else:
+                    new_file.write(old_line)
+    remove(path)
+    move(abs_path, path)
--- src/package_controller/utils/save_version.py
+++ src/package_controller/utils/save_version.py
@@ -1,7 +1,17 @@
-from .save_file import save_file
+from .replace_line import replace_line
 from .find_init_module import find_init_module
+
+RE_VARIABLE = r"^{} = ['\"][^'\"]*['\"]"
+VERSION_VARIABLE = "__version__"
+VERSION_VARIABLE_COMMENTS = [
+    "# Do not change this version manually.\n",
+    "# Versioning is managed by package_controller.\n",
+    "# To update the version run `pc version --patch | --minor | --major`\n",
+]
 
 
 def save_version(next_version):
     init_module = find_init_module()
-    save_file(init_module, "__version__", next_version)
+    pattern = RE_VARIABLE.format(VERSION_VARIABLE)
+    replacement = "{} = \"{}\"".format(VERSION_VARIABLE, next_version)
+    replace_line(init_module, pattern, replacement, VERSION_VARIABLE_COMMENTS)
