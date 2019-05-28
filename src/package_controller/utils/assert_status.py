eb1417e3562b5eaedff9cb1197635ad5ca93e0d6
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-28 02:33:31 -0400

    refactor: Renames module.

--- src/package_controller/utils/assert_status.py
+++ src/package_controller/utils/assert_status.py
@@ -3,9 +3,8 @@ from .run import run
 GIT_STATUS_ARGS = ["git", "status", "-s"]
 
 
-def git_status():
+def assert_status():
     status = run(*GIT_STATUS_ARGS)
     if status:
         raise RuntimeError("There are uncommited changes")
-    return None
 
