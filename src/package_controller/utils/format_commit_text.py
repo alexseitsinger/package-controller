14dfb758a51ddfd7ab2c8ac9628bac8a95e15012
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-28 23:25:31 -0400

    refactor: Only capitalizes first letter of string.

--- src/package_controller/utils/format_commit_text.py
+++ src/package_controller/utils/format_commit_text.py
@@ -1,6 +1,6 @@
 def format_commit_text(text):
     text = text.strip()
-    text = text.capitalize()
+    text = text[0].upper() + text[1:]
     if not text.endswith("."):
         text = "{}.".format(text)
     return text
