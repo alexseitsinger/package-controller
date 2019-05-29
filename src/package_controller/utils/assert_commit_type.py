3153951862a420c89c2e32dfb319c9ebd3f718f3
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-28 20:46:25 -0400

    style: Adds period to end of exception msg.

--- src/package_controller/utils/assert_commit_type.py
+++ src/package_controller/utils/assert_commit_type.py
@@ -6,6 +6,6 @@ COMMIT_TYPES_ALLOWED = [
 
 def assert_commit_type(name):
     if not name in COMMIT_TYPES_ALLOWED:
-        raise AssertionError("The commit type must be one of {}".format(
+        raise AssertionError("The commit type must be one of {}.".format(
                              ", ".join(COMMIT_TYPES_ALLOWED)))
 
