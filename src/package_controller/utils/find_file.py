de521c4558c53265fb8d61e3589bdd4179af92b0
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-30 16:09:21 -0400

    refactor: Adds flat argument.

--- src/package_controller/utils/find_file.py
+++ src/package_controller/utils/find_file.py
@@ -1,16 +1,14 @@
 import os
 
 
-def find_file(file_name, current_dir=None):
+def find_file(file_name, current_dir=None, flat=False):
     if current_dir is None:
         current_dir = os.getcwd()
     file_list = os.listdir(current_dir)
     parent_dir = os.path.dirname(current_dir)
     if file_name in file_list:
         return os.path.join(current_dir, file_name)
-    elif current_dir == parent_dir:
-        raise RuntimeError("Failed to find file {}".format(file_name))
+    elif flat is True or current_dir == parent_dir:
+        raise FileNotFoundError("Failed to find {}".format(file_name))
     else:
         return find_file(file_name, parent_dir)
-
-
