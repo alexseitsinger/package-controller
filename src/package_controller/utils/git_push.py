cf317b6fa16e701607f2ab5bff1cacc5365742e1
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-30 16:42:45 -0400

    refactor: Only pushes single tags.

--- src/package_controller/utils/git_push.py
+++ src/package_controller/utils/git_push.py
@@ -1,15 +1,10 @@
 from .run import run
 from .assert_git_repository import assert_git_repository
 
-GIT_PUSH_ARGS = ["git", "push"]
-GIT_PUSH_TAGS_ARGS = ["--tags"]
 
-
-def git_push(remote="origin", branch="master", tags=False):
+def git_push(remote="origin", branch="master", tag_name=None):
     assert_git_repository()
-    if tags is True:
-        args = GIT_PUSH_ARGS + GIT_PUSH_TAGS_ARGS
-    else:
-        args = GIT_PUSH_ARGS + [remote, branch]
-    return run(*args)
+    run("git", "push", remote, branch)
+    if tag_name is not None:
+        run("git", "push", remote, tag_name)
 
