73547241966a15c4b5998cc4fb418e5e361067a7
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-05-30 08:13:42 -0400

    refactor: Tries multiple filenames for whl/tars.

--- src/package_controller/utils/twine_upload.py
+++ src/package_controller/utils/twine_upload.py
@@ -17,19 +17,38 @@ def twine_upload():
     setup_module = find_file("setup.py")
     root = os.path.dirname(setup_module)
     directory_name = os.path.basename(root)
-    package_name = directory_name.replace("-", "_")
     dist_dir = os.path.join(root, "dist")
-    wheel_name = WHEEL_NAME.format(package_name, version)
-    wheel = os.path.join(dist_dir, wheel_name)
-    tarball_name = TARBALL_NAME.format(package_name, version)
-    tarball = os.path.join(dist_dir, tarball_name)
-    uploaded = [wheel, tarball]
-    for path in uploaded:
-        if not os.path.exists(path):
-            raise FileNotFoundError("File does not exist. ({})".format(path))
+    if not os.path.isdir(dist_dir):
+        raise NotADirectoryError("Dist directory does not exist.")
+    package_names = [
+        directory_name,
+        directory_name.replace("-", "_"),
+    ]
+    wheel_names = [
+        WHEEL_NAME.format(x, version) for x in package_names
+    ]
+    tarball_names = [
+        TARBALL_NAME.format(x, version) for x in package_names
+    ]
+    wheel = None
+    for wheel_name in wheel_names:
+        if wheel is None:
+            wheel_path = os.path.join(dist_dir, wheel_name)
+            if os.path.exists(wheel_path):
+                wheel = wheel_path
+    tarball = None
+    for tarball_name in tarball_names:
+        if tarball is None:
+            tarball_path = os.path.join(dist_dir, tarball_name)
+            if os.path.exists(tarball_path):
+                tarball = tarball_path
+    built = [wheel, tarball]
+    for built_file in built:
+        if built_file is None or not os.path.exists(built_file):
+            raise FileNotFoundError("File does not exist. ({})".format(built_file))
     # Check if Twine exists on PATH.
     assert_which("twine")
     # Run the command
-    run(*TWINE_UPLOAD_ARGS + uploaded)
+    run(*TWINE_UPLOAD_ARGS + built)
     # Return the list of paths we uploaded.
-    return uploaded
+    return built
