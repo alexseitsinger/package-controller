bd1ee7b5887bd3411f47196115f2fa1f75dac9aa
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-13 20:21:49 -0400

    feat: Adds get_python_package_version util.

---
+++ src/package_controller/library/python/get_python_package_version.py
@@ -0,0 +1,18 @@
+import re
+
+from ..generic.pipe_commands import pipe_commands
+
+
+def get_python_package_version(name):
+    output = pipe_commands(["pipenv graph", "grep {}".format(name)])
+    try:
+        if output.startswith("  - "):
+            pattern = r"installed: ([\d.]+)"
+            m = re.search(pattern, output)
+            version = m.groups()[0]
+        else:
+            bits = output.split("==")
+            version = bits[1]
+        return version.strip()
+    except IndexError:
+        raise RuntimeError("Failed to get python package version. ({})".format(name))
