a63b3342c35ba9be5a9d85045abce40d1682c7d0
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-17 13:07:23 -0400

    feat: Makes yarn release non-interactive.

--- src/package_controller/cli/commands/release.py
+++ src/package_controller/cli/commands/release.py
@@ -14,22 +14,36 @@ FAILURE_EXCEPTIONS = (
 
 
 @click.command()
-@click.option("--remote", "-r", default="origin", required=False, help="The remote to push to.")
-@click.option("--branch", "-b", default="master", required=False, help="The branch to push to.")
-@click.option("--no-tag", "-nt", default=False, required=False, is_flag=True, help="If true, will not push the tag.")
-def release(remote, branch, no_tag):
+@click.option(
+    "--remote", "-r", default="origin", required=False, help="The remote to push to."
+)
+@click.option(
+    "--branch", "-b", default="master", required=False, help="The branch to push to."
+)
+@click.option("--otp", required=False, help="The OTP code for NPM.")
+@click.option(
+    "--no-tag",
+    "-nt",
+    default=False,
+    required=False,
+    is_flag=True,
+    help="If true, will not push the tag.",
+)
+def release(remote, branch, otp, no_tag):
     try:
         tag_name = None
         if no_tag is False:
             tag_name = "v{}".format(get_version())
-        uploaded = release_package(remote=remote, branch=branch, tag_name=tag_name)
+        uploaded = release_package(
+            remote=remote, branch=branch, tag_name=tag_name, otp=otp
+        )
         click.secho("Successfully released package.", fg="green", bold=True)
         if uploaded is not None:
             click.secho(
-                "Files:\n    {}".format("\n    ".join([
-                    os.path.basename(x) for x in uploaded
-                ])),
-                fg="green"
+                "Files:\n    {}".format(
+                    "\n    ".join([os.path.basename(x) for x in uploaded])
+                ),
+                fg="green",
             )
     except FAILURE_EXCEPTIONS as exc:
         message = str(exc)
--- src/package_controller/library/fascades/release_package.py
+++ src/package_controller/library/fascades/release_package.py
@@ -7,22 +7,25 @@ from ..git.push import push
 from ..node.is_node_package import is_node_package
 from ..python.is_python_package import is_python_package
 from ..python.twine_upload import twine_upload
+from ..generic.run_interactive import run_interactive
 
 
-def release_package_node():
+def release_package_node(otp=None):
     assert_which("node")
     # get the package info
     package_file = find_file("package.json")
     package_name = None
+    package_version = None
     with open(package_file, "r") as f:
         package_file_json = json.loads(f.read())
         package_name = package_file_json["name"]
-    try:
-        assert_which("yarn")
-        run("yarn publish --access public")
-    except AssertionError:
-        assert_which("npm")
-        run("npm publish --access public")
+        package_version = package_file_json["version"]
+    cmd = "yarn publish --non-interactive --access public --new-version {}".format(
+        package_version
+    )
+    if otp is not None:
+        cmd = "{} --otp {}".format(cmd, otp)
+    run(cmd)
     return [package_name]
 
 
@@ -31,14 +34,14 @@ def release_package_python():
     return twine_upload()
 
 
-def release_package(remote="origin", branch="master", tag_name=None):
+def release_package(remote="origin", branch="master", tag_name=None, otp=None):
     is_python = is_python_package()
     is_node = is_node_package()
     push(remote=remote, branch=branch, tag_name=tag_name)
     if is_python and not is_node:
         return release_package_python()
     elif is_node and not is_python:
-        return release_package_node()
+        return release_package_node(otp=otp)
     elif is_python and is_node:
         raise RuntimeError("Both python and node packages detected.")
     else:
