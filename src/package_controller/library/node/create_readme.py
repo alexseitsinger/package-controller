8bf5bb63f5b0456d178058ce0982a7aac6ff14a3
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-23 16:40:49 -0400

    refactor: Adds root_dir argument.

--- src/package_controller/library/node/create_readme.py
+++ src/package_controller/library/node/create_readme.py
@@ -5,9 +5,8 @@ from ..generic.run import run
 COMMAND = "documentation build {input_file} -f {output_format} -o {output_file}"
 
 
-def create_readme(input_path, output_name):
-    root_dir = os.path.dirname(input_path)
-    output_path = (os.path.join(root_dir, output_name),)
+def create_readme(root_dir, input_path, output_name):
+    output_path = os.path.join(root_dir, output_name)
     run(
         COMMAND.format(
             input_file=input_path, output_format="md", output_file=output_path
