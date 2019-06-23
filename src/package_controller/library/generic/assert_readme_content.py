70a6844c372c585130d115a79e43dd698031199c
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-23 16:41:18 -0400

    fix: Fixes line counting.

--- src/package_controller/library/generic/assert_readme_content.py
+++ src/package_controller/library/generic/assert_readme_content.py
@@ -2,6 +2,9 @@ from io import open
 
 
 def assert_readme_content(readme_file):
+    total = 0
     with open(readme_file, "r") as f:
-        if len(f.readlines()) <= 2:
-            raise AssertionError("The README has no content.")
+        for line in f:
+            total += 1
+    if total <= 3:
+        raise AssertionError("The README has no content.")
