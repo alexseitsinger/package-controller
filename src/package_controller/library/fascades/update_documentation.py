557a84d5658b0a81a6f985a3d4486fd970f20989
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-23 16:46:10 -0400

    docs: Updates comments for function.

--- src/package_controller/library/fascades/update_documentation.py
+++ src/package_controller/library/fascades/update_documentation.py
@@ -13,13 +13,20 @@ README_NAME = "README.md"
 def update_documentation_node():
     assert_which("documentation")
     root_dir = os.getcwd()
+    # Find the package's index file, or raise an exception.
     index_file = find_index_file(root_dir)
+    # Attempt to create the README from the index file first.
     readme_file = create_readme(root_dir, index_file, README_NAME)
+    # Check if the created README is empty...
     try:
         assert_readme_content(readme_file)
     except AssertionError:
+        # If it is empty, attempt to create another README from the src
+        # directory instead of just the index file.
         print("Creating {} from entire src directory.".format(README_NAME))
         readme_file = create_readme(root_dir, "src/**", README_NAME)
+        # If it's still empty, raise another exception.
+        assert_readme_content(readme_file)
     return readme_file
 
 
