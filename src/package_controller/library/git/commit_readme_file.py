9433bb0e34ec4088db7ba3b53ede1faca769f7be
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-23 21:24:11 -0400

    fix: Fixes import for assert_status.

--- src/package_controller/library/git/commit_readme_file.py
+++ src/package_controller/library/git/commit_readme_file.py
@@ -1,7 +1,7 @@
-from ..generic.assert_status import assert_status
-from ..fascades.get_version import get_version
-from .commit import commit
 from .add import add
+from .commit import commit
+from .assert_status import assert_status
+from ..fascades.get_version import get_version
 
 
 def commit_readme_file(relative_readme_path):
