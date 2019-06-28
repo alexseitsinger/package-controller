1ec1cab9191b0c9919cf3b40910c8a9fb340b076
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-27 21:14:19 -0400

    refactor: Trys making docs from dir, then index.

--- src/package_controller/library/fascades/update_documentation.py
+++ src/package_controller/library/fascades/update_documentation.py
@@ -14,11 +14,15 @@ README_NAME = "README.md"
 def update_documentation_node(status_message):
     assert_which("documentation")
     root_dir = os.getcwd()
-    # Find the package's index file, or raise an exception.
+    input_file = "src/**"
+    # If there is no source directory, try to find an index file.
     if not os.path.isdir(os.path.join(root_dir, "src")):
-        raise RuntimeError("There is no src directory.")
+        input_file = find_index_file(root_dir)
+        # If there is no index file, raise an exception.
+        if not os.path.isfile(input_file):
+            raise FileNotFoundError("There is no src directory or index file.")
     # Attempt to create the README from the index file first.
-    readme_file = create_readme(root_dir, "src/**", README_NAME)
+    readme_file = create_readme(root_dir, input_file, README_NAME)
     # Check if the created README is empty...
     assert_readme_content(readme_file)
     # Commit the file to git.
