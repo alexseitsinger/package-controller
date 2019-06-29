4c3773e98b250a12c099057e7f39f4439b56e8f7
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-29 15:12:54 -0400

    feat: Adds save command.

---
+++ src/package_controller/cli/commands/save.py
@@ -0,0 +1,29 @@
+import os
+import click
+
+from ...library.git.add import add
+from ...library.git.commit import commit
+
+FAILURE_EXCEPTIONS = (
+    RuntimeError,
+    AssertionError,
+    AttributeError,
+    FileNotFoundError,
+    NotADirectoryError,
+)
+
+
+@click.command(name="save")
+@click.option("--type", "-t", "type_", required=True, help="The commit type.")
+@click.option("--message", "-m", required=True, help="The commit message.")
+@click.argument("files", required=True, nargs=-1)
+def save_command(type_, message, files):
+    try:
+        staged = add(*files)
+        commit_hash = commit(type_, message)
+        click.secho("Successfully saved files.", fg="green", bold=True)
+        click.secho("Commit: {}".format(commit_hash), fg="green")
+        click.secho("Files:\n    {}".format("\n    ".join(list(staged))), fg="green")
+    except FAILURE_EXCEPTIONS as exc:
+        click.secho("Failed to save files.", fg="red", bold=True)
+        click.secho(str(exc), fg="red")
--- src/package_controller/cli/main.py
+++ src/package_controller/cli/main.py
@@ -10,6 +10,7 @@ from .commands.unpin import unpin
 from .commands.release import release
 from .commands.test import test
 from .commands.version import version
+from .commands.save import save_command
 
 
 @click.group()
@@ -27,3 +28,4 @@ main.add_command(unpin)
 main.add_command(release)
 main.add_command(test)
 main.add_command(version)
+main.add_command(save_command)
