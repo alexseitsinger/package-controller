748b3cdd8a5fd961f1863670c4ed193e9db9887c
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-29 15:31:26 -0400

    refactor: Removes exception for no file names.

--- src/package_controller/library/git/add.py
+++ src/package_controller/library/git/add.py
@@ -4,6 +4,4 @@ from .assert_repository import assert_repository
 
 def add(*files):
     assert_repository()
-    if not len(files):
-        raise RuntimeError("No files passed")
     return [add_file(x) for x in files]
