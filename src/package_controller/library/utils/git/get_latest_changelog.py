918f677d0e84414327d3ca8e5a6ae708b8c85661
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-30 14:46:57 -0400

    fix: Adds exception handler for empty changelogs.

--- src/package_controller/library/utils/git/get_latest_changelog.py
+++ src/package_controller/library/utils/git/get_latest_changelog.py
@@ -7,7 +7,9 @@ def get_latest_changelog(tag_name):
     cmd = "git-changelog -t angular -s angular ."
     out = run(cmd)
     section = out.split("\n\n\n")[0]
-    heading, body = section.split("\n\n", 1)
-    repl_heading = re.sub(r"v(\.?\d+)+", tag_name, heading)
-    final = "\n\n".join([repl_heading, body])
-    return final
+    try:
+        heading, body = section.split("\n\n", 1)
+        repl_heading = re.sub(r"v(\.?\d+)+", tag_name, heading)
+        return "\n\n".join([repl_heading, body])
+    except ValueError:
+        return section
