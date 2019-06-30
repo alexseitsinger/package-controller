ce66c23e0762deafddf10eaaa9f8ba8192682f64
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-06-30 09:21:14 -0400

    refactor: Changes arguments.

--- src/package_controller/library/git/push.py
+++ src/package_controller/library/git/push.py
@@ -5,11 +5,11 @@ from .assert_repository import assert_repository
 COMMAND = "git push {force} {remote} {branch}"
 
 
-def push(remote="origin", branch="master", tag_name=None, force=False):
+def push(remote="origin", branch="master", tag=None, force=False):
     assert_repository()
     if force is True:
         run(COMMAND.format(force="-f", remote=remote, branch=branch))
     else:
         run(COMMAND.format(force="", remote=remote, branch=branch))
-    if tag_name is not None:
-        run(COMMAND.format(force="", remote=remote, branch=tag_name))
+    if tag is not None:
+        run(COMMAND.format(force="", remote=remote, branch=tag))
