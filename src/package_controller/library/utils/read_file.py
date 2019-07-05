eb0f921c07db2dac4196994c7d4961e0048dafbc
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-07-05 14:35:07 -0400

    refactor: Read_file uses variable for python only.

--- src/package_controller/library/utils/read_file.py
+++ src/package_controller/library/utils/read_file.py
@@ -1,5 +1,7 @@
 import re
+import json
 from io import open
+
 
 RE_VARIABLE = r"^{} = ['\"]([^'\"]*)['\"]"
 
@@ -7,10 +9,18 @@ RE_VARIABLE = r"^{} = ['\"]([^'\"]*)['\"]"
 def read_file(path, variable=None):
     with open(path, "r") as f:
         content = f.read()
-        if variable is None:
+        if path.endswith(".py"):
+            if variable is None:
+                return content
+            regex = RE_VARIABLE.format(variable)
+            match = re.search(regex, content, re.M)
+            if match:
+                return match.group(1)
+            raise AttributeError("{} was not found in {}".format(variable, path))
+        elif path.endswith(".json"):
+            data = json.loads(content)
+            if variable is None:
+                return data
+            return data[variable]
+        else:
             return content
-        regex = RE_VARIABLE.format(variable)
-        match = re.search(regex, content, re.M)
-        if match:
-            return match.group(1)
-        raise AttributeError("{} does not exist in {}".format(variable, path))
