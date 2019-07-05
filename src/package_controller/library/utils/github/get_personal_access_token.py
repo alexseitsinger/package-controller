9968f65a002bd2204096cacf9f390dad5854b517
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-07-05 14:35:39 -0400

    refactor: Changes for get_config module api.

--- src/package_controller/library/utils/github/get_personal_access_token.py
+++ src/package_controller/library/utils/github/get_personal_access_token.py
@@ -1,18 +1,24 @@
 import os
 import json
 
-from ..get_package_controller_file import get_package_controller_file
+from ..get_config_file import get_config_file
 
 
 def get_personal_access_token(username):
     token = os.environ.get("GITHUB_PERSONAL_ACCESS_TOKEN", None)
     if token is None:
-        controller_file = get_package_controller_file()
+        config_file = get_config_file()
         # Read the cred file for our remote url.
-        with open(controller_file, "r") as fr:
+        with open(config_file, "r") as fr:
+            data = json.loads(fr.read())
             try:
-                data = json.loads(fr.read())
-                token = data[username]["personal_access_token"]
+                tokens = data["tokens"]
+            except AttributeError:
+                tokens = data["tokens"] = {}
+                with open(config_file, "w") as fw:
+                    fw.write(json.dumps(data))
+            try:
+                token = tokens[username]
             except AttributeError:
                 raise AttributeError(
                     "The username ({}) has no personal access token saved.".format(
