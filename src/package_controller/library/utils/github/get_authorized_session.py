0cf3d1db9eab0f0db0b91fd4e6a1184df22048f3
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-07-05 14:34:14 -0400

    refactor: Adds headers to all sessions.

--- src/package_controller/library/utils/github/get_authorized_session.py
+++ src/package_controller/library/utils/github/get_authorized_session.py
@@ -5,6 +5,9 @@ from .get_personal_access_token import get_personal_access_token
 
 def get_authorized_session(username, password=None):
     session = requests.Session()
+    session.headers.update(
+        {"Content-Type": "application/json", "Accept": "application/json"}
+    )
     if password is None:
         token = get_personal_access_token(username)
         session.headers.update({"Authorization": "token {}".format(token)})
