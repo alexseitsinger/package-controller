1d95895546df66d1601b9d02a80ac759adea95c5
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-07-05 20:55:28 -0400

    refactor: Adds unsave,resave,undo commands.

--- src/package_controller/cli/main.py
+++ src/package_controller/cli/main.py
@@ -4,10 +4,9 @@ from .commands.version import version_command
 from .commands.bump import bump_command
 from .commands.diff import diff_command
 from .commands.save import save_command
-
-# from .commands.unsave import unsave_command
-# from .commands.undo import undo_command
-# from .commands.resave import resave_command
+from .commands.unsave import unsave_command
+from .commands.undo import undo_command
+from .commands.resave import resave_command
 from .commands.test import test_command
 from .commands.build import build_command
 from .commands.document import document_command
@@ -26,9 +25,9 @@ main.add_command(version_command)
 main.add_command(bump_command)
 main.add_command(diff_command)
 main.add_command(save_command)
-# main.add_command(unsave_command)
-# main.add_command(undo_command)
-# main.add_command(resave_command)
+main.add_command(unsave_command)
+main.add_command(undo_command)
+main.add_command(resave_command)
 main.add_command(test_command)
 main.add_command(build_command)
 main.add_command(document_command)
